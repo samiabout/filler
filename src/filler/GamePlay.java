6918a2b3c5c47af43b5b32c577eb2a6de3bc310a
samiabout <sami-about@hotmail.fr>
2016-06-06 17:48:11 +0200

    p

--- src/filler/GamePlay.java
+++ src/filler/GamePlay.java
@@ -8,28 +8,9 @@ public class GamePlay {
 	long allGameTime = 0;
 	static Scanner sc  = new Scanner(System.in);
 	
-<<<<<<< HEAD
-<<<<<<< HEAD
-	public int nbPlayers;
-	public int length;
-	public int height;
-	
-	public boolean hexagonal;
-	public boolean islet;
-	public boolean obstacles;
-	public double obstaclesAmount;
-	public boolean getOldSave;
-	public MyConnector aConnection=new MyConnector(true);
-	
-
-	public boolean[] playerIA; 
-	public int[] ialevel;//ia difficile ou non
-=======
-=======
->>>>>>> parent of 199a38d... ia
 	public int nbPlayers=2;
-	public int length=10;
-	public int height=10;
+	public int length=20;
+	public int height=20;
 	
 	public boolean hexagonal=false;
 	public boolean islet=false;
@@ -39,15 +20,11 @@ public class GamePlay {
 	public MyConnector aConnection=new MyConnector(true);
 	
 
-	public boolean playerIA[]={false,false}; 
-	public boolean ialevel[]={true,true};//ia difficile ou non
-<<<<<<< HEAD
->>>>>>> parent of 199a38d... ia
-=======
->>>>>>> parent of 199a38d... ia
-	public boolean playerConnected[]={true,false};
+	public boolean playerIA[]={true,true,false,false}; 
+	public int ialevel[]={4,4,1,1};//ia difficile ou non
+	public boolean playerConnected[]={false,true};
 		//doit transmettre le coup
-
+	
 	//boolean opponentConnected[]={false,true};//doit transmettre le coup
 	public Interface interfaceG= new Interface();
 	public Player[] tabJeu=new Player[4];
@@ -55,19 +32,7 @@ public class GamePlay {
 	public Save oldSave=new Save();
 	public int noTour=0 ;
 	
-	public GamePlay(int nbPlayers, int length, int height, boolean hexagonal, boolean islet, boolean obstacles, 
-			double obstaclesAmount, boolean getOldSave, boolean[] playerIA, int[] ialevel) {
-		this. nbPlayers = nbPlayers;
-		this.length = length;
-		this.height = height;
-		this.hexagonal = hexagonal;
-		this.islet = islet;
-		this.obstacles = obstacles;
-		this.obstaclesAmount = obstaclesAmount;
-		this.getOldSave = getOldSave;
-		this.playerIA = playerIA;
-		this.ialevel = ialevel;
-		
+	public GamePlay() {
 		this.parameterize();
 		this.allGameTime = System.currentTimeMillis();
 	}
@@ -82,8 +47,6 @@ public class GamePlay {
 			System.out.println("recupérer un sauvegarde? (true/false)");
 			getOldSave=sc.nextBoolean();
 			if(!getOldSave){
-<<<<<<< HEAD
-<<<<<<< HEAD
 					Main.playConnected=false;
 					playerIA[0]=false;playerIA[1]=false;
 					System.out.println("Choisir le nombre de joueurs (2, 3 ou 4) ");
@@ -99,52 +62,20 @@ public class GamePlay {
 								if(i<2 && nbPlayers<=2){
 									System.out.println("(0 à 3)");
 								}else{
-									System.out.println("(0 ou 1)");
+									System.out.println("(0 à 2)");
 								}
 								ialevel[i]=sc.nextInt();
 							}
 						}
 					}
 			
-=======
-=======
->>>>>>> parent of 199a38d... ia
-				Main.playConnected=false;
-				playerIA[0]=false;playerIA[1]=false;
-				System.out.println("Choisir le nombre de joueurs (2, 3 ou 4) ");
-			nbPlayers = sc.nextInt();
-			if(nbPlayers==2){
-				playerIA[0]=false;playerIA[1]=false;
-				System.out.println("voulez vous une IA (0,1,2)");
-				int nbIA=sc.nextInt();
-				if (nbIA==2){
-					playerIA[0]=true;playerIA[1]=true;
-					System.out.println("level IA 1 difficile? (true,false)");
-					ialevel[0]=sc.nextBoolean();
-					System.out.println("level IA 2 difficile? (true,false)");
-					ialevel[1]=sc.nextBoolean();
 					
-				}
-				if(nbIA==1){
-					System.out.println("Joueur 1 ou 2? (1,2)");
-					int plIA=sc.nextInt();
-					if (plIA==2){
-						playerIA[1]=true;playerIA[0]=false;
-					}
-					else{
-						playerIA[0]=true;playerIA[1]=false;
-					}
-					ialevel[0]=false;ialevel[1]=false;
-					System.out.println("IA difficile? (true,false)");
-						boolean level=sc.nextBoolean();
-						ialevel[0]=playerIA[0]&&level;
-						ialevel[1]=playerIA[1]&&level;
-				}
-			}
-<<<<<<< HEAD
->>>>>>> parent of 199a38d... ia
-=======
->>>>>>> parent of 199a38d... ia
+					
+				
+
+			
+			
+			
 
 				System.out.println("Choisir la taille du plateau(hauteur puis longueur )");
 			height=sc.nextInt();
@@ -169,16 +100,12 @@ public class GamePlay {
 		else {
 			board=new Board(height,length,nbPlayers,hexagonal,obstacles,obstaclesAmount,islet,aConnection);
 		}
-		
-		if(!getOldSave){
-			board=new Board(height,length,nbPlayers,hexagonal,obstacles,obstaclesAmount,islet,aConnection);
-		} else {
-			oldSave=Save.getSave();
-		}
 	
-		if(!getOldSave){
+		if(!getOldSave && !Main.playConnected ){
+			
 			
 			interfaceG= new Interface(board,2.5);
+			if(!Main.autoset){
 			System.out.println("do you want to change the board (true,false)");
 			if(sc.nextBoolean()){
 				interfaceG.creatBoard(board.height(), board.length());
@@ -190,25 +117,20 @@ public class GamePlay {
 				}
 			}
 		}
+		}
 	
 	 
 	
+	for (int i = 0; i < nbPlayers; i++) {
+		if (playerIA[i]){
+			tabJeu[i]=new IA(i+1,nbPlayers, interfaceG);
+		}
+		else{
+			tabJeu[i]=new Player(i+1,nbPlayers, interfaceG);
+		}
+	}	
+	 
 
-	 
-	 if(playerIA[0]){
-		 tabJeu[0]=new IA(1,nbPlayers, interfaceG);
-	 }
-	 else{
-		 tabJeu[0]=new Player(1,nbPlayers, interfaceG);
-	 }
-	 if(playerIA[1]){
-		 tabJeu[1]=new IA(2,nbPlayers, interfaceG);
-	 }
-	 else{
-		 tabJeu[1]=new Player(2,nbPlayers, interfaceG);
-	 }
-	tabJeu[2]=new Player(3,nbPlayers, interfaceG);
-	tabJeu[3]=new Player(4,nbPlayers, interfaceG);
 	
 	
 	if(!getOldSave){
@@ -263,33 +185,12 @@ public class GamePlay {
 		game.board.setCouleur(choix,game.tabJeu[i], game.interfaceG);
 		game.board.toMaj();	
 		return false;
-	}	
-	
-	public void setHexagonal(boolean hexagonal){
-		this.hexagonal = hexagonal;
 	}
 	
-	public void setIslet(boolean islet){
-		this.islet = islet;
-	}
 	
-	public void setObstacles(boolean obstacles){
-		this.obstacles = obstacles;
-	}
 	
-	public void setLength(int length){
-		this.getOldSave = getOldSave;
-	}
 	
-	public void setHeight(int height){
-		this.getOldSave = getOldSave;
-	}
 	
-	public void setGetOldSave(boolean getOldSave){
-		this.getOldSave = getOldSave;
-	}
 	
-	public void setObstaclesAmount(double obstaclesAmount){
-		this.obstaclesAmount = obstaclesAmount;
-	}
+	
 }
